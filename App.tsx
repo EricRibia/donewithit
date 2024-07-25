fb866f94706d572a10339be02d2a2af75b273904
ericribia <eric.ndungu16@gmail.com>
2024-07-25 13:14:51 +0300

    chore: setup and views and safearea view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [numberOfLines, setNumberOfLines] = useState(1)
+  let x:string = 'eric';
+
+  const updateNumberOfLines = () => {
+    if(numberOfLines === 1){
+      setNumberOfLines(10)
+    }else{
+      setNumberOfLines(1)
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+      <Text onPress={()=>updateNumberOfLines()} numberOfLines={numberOfLines} style={styles.text}>Hello Eric, let us see how text is wrapped around here and why because the text is quite long btw, let us see how text is wrapped around here and why because the text is quite long btw, let us see how text is wrapped around here and why because the text is quite long btw</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +27,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  contentContainer:{
+    padding: 10
+  },
+  text:{
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'rgba(157,157,157,0.56)',
+      borderRadius: 8,
+  }
 });
