ade46bd4d467dd8e2d4fd76579966f350d6c4fbe
ericribia <eric.ndungu16@gmail.com>
2024-07-25 14:32:19 +0300

    chore: images network and static

--- App.tsx
+++ App.tsx
@@ -1,24 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import {useState} from "react";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Image,
+  Button,
+  Platform,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  const [numberOfLines, setNumberOfLines] = useState(1)
-  let x:string = 'eric';
+  const [numberOfLines, setNumberOfLines] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const updateNumberOfLines = () => {
-    if(numberOfLines === 1){
-      setNumberOfLines(10)
-    }else{
-      setNumberOfLines(1)
+    if (numberOfLines === 1) {
+      setNumberOfLines(10);
+    } else {
+      setNumberOfLines(1);
     }
-  }
+  };
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.contentContainer}>
-      <Text onPress={()=>updateNumberOfLines()} numberOfLines={numberOfLines} style={styles.text}>Hello Eric, let us see how text is wrapped around here and why because the text is quite long btw, let us see how text is wrapped around here and why because the text is quite long btw, let us see how text is wrapped around here and why because the text is quite long btw</Text>
+      <View
+        style={[
+          styles.contentContainer,
+          { paddingTop: Platform.OS === "android" ? 40 : 0 },
+        ]}
+      >
+        <Image
+          style={styles.sourImageStyles}
+          source={require("./assets/events-header.png")}
+        />
+        <Button title="Load Data" onPress={() => fetchData()} />
+        <Image
+          style={styles.networkImageStyles}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/500/500",
+          }}
+        />
       </View>
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -26,15 +64,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  contentContainer:{
-    padding: 10
+  sourImageStyles: {
+    marginVertical: 8,
+    width: "100%",
+    resizeMode: "cover",
+    height: 200,
+    borderRadius: 20,
   },
-  text:{
-      padding: 10,
-      borderWidth: 1,
-      borderColor: 'rgba(157,157,157,0.56)',
-      borderRadius: 8,
-  }
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  networkImageStyles: {
+    marginVertical: 8,
+    resizeMode: "cover",
+    width: "100%",
+    borderRadius: 20,
+  },
 });
