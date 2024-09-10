59d33a6f5935a784c0040058526f6109b2317d49
ericribia <eric.ndungu16@gmail.com>
2024-09-10 13:24:57 +0300

    chore: navigation, stack and tab navigation

--- App.tsx
+++ App.tsx
@@ -1,56 +1,91 @@
-import { Image, StyleSheet, View } from "react-native";
-import { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
 import Screen from "./components/Screen";
 import AppText from "./components/AppText";
 import AppButton from "./components/AppButton";
-import ImageInput from "./components/ImageInput";
-import ImageInputList from "./components/ImageInputList";
-import ListingEditScreen from "./Screens/ListingEditScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Colors from "./utils/colors";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-];
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <AppText>Tweets Section</AppText>
+      <AppButton
+        label={"View Tweets"}
+        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = ({ route }) => {
+  return (
+    <Screen>
+      <AppText>Tweet Details:{route.params.id}</AppText>
+    </Screen>
+  );
+};
+
+const Account = () => {
+  return (
+    <Screen>
+      <AppText>Account Screen</AppText>
+    </Screen>
+  );
+};
+
+export type RootStackParamList = {
+  Tweets: undefined; // No parameters
+  TweetDetails: { id: number }; // Parameters for Details screen
+};
+const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveBackgroundColor: "tomato",
+        tabBarActiveTintColor: "white",
+        tabBarInactiveBackgroundColor: "#eee",
+        tabBarInactiveTintColor: "black",
+      }}
+    >
+      <Tab.Screen
+        name={"Feed"}
+        component={Tweets}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name={"home"} size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen name={"Account"} component={Account} />
+    </Tab.Navigator>
+  );
+};
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName={"Tweets"}>
+      <Stack.Screen name={"Tweets"} component={Tweets} />
+      <Stack.Screen
+        name={"TweetDetails"}
+        component={TweetDetails}
+        options={({ route }) => ({
+          title: `Details:${route.params.id}`,
+          headerStyle: { backgroundColor: Colors.primary },
+          headerTintColor: "white",
+          headerShown: false,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 export default function App() {
-  const [imageUris, setImageUris] = useState<{ id: string; uri: string }[]>([]);
-  const [category, setCategory] = useState("Furniture");
-  const addImage = (imageUri: string) => {
-    setImageUris([
-      ...imageUris,
-      {
-        id: generateUniqueId(),
-        uri: imageUri,
-      },
-    ]);
-  };
-  const removeImage = (imageId: string) => {
-    const updatedList = imageUris.filter((imgData) => imgData.id !== imageId);
-    setImageUris(updatedList);
-  };
-  const generateUniqueId = () => {
-    const id = Math.floor(1000 + Math.random() * 9000); // Generates a number between 1000 and 9999
-    return id.toString(); // Convert the number to a string
-  };
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      {/*<StackNavigator />*/}
+      <TabNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingTop: 10,
-    display: "flex",
-    flexDirection: "row",
-  },
-});
